53e823ff0804185e1a3f787ac8a6e1f38586ffef
janmikael <michaelkimu446@gmail.com>
2024-05-12 03:19:18 +0800

    PriceInfoCards

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -9,6 +9,9 @@ import Image from "next/image";
 
 // Importing the Link component from the "next/link" module.
 import Link from "next/link";
+import { Product } from "@/types";
+import { formatNumber } from "@/lib/util";
+import PriceInfoCard from "@/components/PriceInfoCard";
 
 // Defining the Props type, which is an object with a params property of type { id: string }.
 type Props = {
@@ -18,7 +21,7 @@ type Props = {
 // Defining the ProductDetails component as an asynchronous function that takes Props as a parameter.
 const ProductDetails = async ({ params: { id } }: Props) => {
   // Fetching the product details based on the provided id.
-  const product = await getProductById(id);
+  const product: Product = await getProductById(id);
 
   // Redirecting to the homepage if the product is not found.
   if (!product) redirect('/');
@@ -55,9 +58,136 @@ const ProductDetails = async ({ params: { id } }: Props) => {
                 Visit Product
               </Link>
             </div>
+
+            <div className="flex items-center gap-3">
+
+            <div className="product-hearts">
+            <Image 
+                  src="/assets/icons/red-heart.svg"
+                  alt="heart"
+                  width={20}
+                  height={20}
+                />
+
+                  <p className="text-base font-semibold text-[#D46F77]">
+                  {product.reviewsCount || '100'}
+                </p>
+            </div>
+
+            <div className="p-2 bg-white-200 rounded-10">
+
+              <Image
+                src="/assets/icons/bookmark.svg"
+                alt="bookmark"
+                width={20}
+                height={20}
+              />
+
+            </div>
+
+            <div className="p-2 bg-white-200 rounded-10">
+
+              <Image
+                src="/assets/icons/share.svg"
+                alt="share"
+                width={20}
+                height={20}
+              />
+
+            </div>
+
+
+
+            </div>
+
           </div>
+
+          <div className="product-info">
+            <div className="flex flex-col gap-2">
+              <p className="text-[34] text-secondary font-bold">
+                {product.currency} {formatNumber(product.currentPrice)}
+                </p>
+
+                <p className="text-[21] text-secondary font-bold opacity-50 line-through">
+                {product.currency} {formatNumber(product.originalPrice)}
+                </p>
+            </div>
+
+            <div className="flex flex-col gap-4"> 
+              <div className="flex gap-3">
+                <div className="product-stars">
+                  <Image
+                    src="/assets/icons/star.svg"
+                    alt="star"
+                    width={16}
+                    height={16}
+                  />
+                  <p className="text-sm text-primary-orange font-semibold">
+                    {product.stars || '25'}
+                  </p>
+                </div>
+
+                <div className="product-reviews">
+                  <Image
+                  src="/assets/icons/comment.svg"
+                  alt="comment"
+                  width={16}
+                  height={16}
+                  />
+                  <p className="text-sm text-secondary font-semibold">
+                    {product.reviewsCount || '100'} Reviews
+                  </p>
+                </div>
+              </div>
+
+              <p className="text-sm text-black opacity-50">
+                <span className="text-primary-green font-semibold">93% </span> of buyers have recommended this.
+              </p>
+            </div>
+
+          </div>
+
+            <div className="m-7 flex flex-col gap-5">
+              <div className="flex gap-5 flex-wrap">
+                
+                
+                <PriceInfoCard
+                  title="Current Price"
+                  iconSrc="/assets/icons/price-tag.svg"
+                  value={`${product.currency} ${formatNumber(product.currentPrice)}`}
+               
+                />
+
+                <PriceInfoCard
+                  title="Average Price"
+                  iconSrc="/assets/icons/chart.svg"
+                  value={`${product.currency} ${formatNumber(product.averagePrice)}`}
+                
+                />
+
+                <PriceInfoCard
+                  title="Current Price"
+                  iconSrc="/assets/icons/arrow-up.svg"
+                  value={`${product.currency} ${formatNumber(product.highestPrice)}`}
+                 
+                />
+
+                <PriceInfoCard
+                  title="Current Price"
+                  iconSrc="/assets/icons/arrow-down.svg"
+                  value={`${product.currency} ${formatNumber(product.lowestPrice)}`}
+                
+                />
+
+              </div>
+            </div>
+
+            Modal
+
         </div>
       </div>
+
+
     </div>
   );
 };
