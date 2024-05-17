2a7fb6f776edcb218e6dd1b0edc6f6c14405f4ba
janmikael <iamrealmichaelpacto446@gmail.com>
2024-05-17 13:38:43 +0800

    fix typo in nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
             />
 
             <p className='nav-logo'>
-                Price<span className='text-primary'>Wise</span>
+                Price<span className='text-primary'>Tracker</span>
             </p>
           
         </Link>
